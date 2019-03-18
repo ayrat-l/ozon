e914f507e42459d6b2aa9db6d267c5456c707818
ayrat-l <kaz90@bk.ru>
2019-03-18 21:40:07 +0300

    Added test for def add_book():

--- tests/test_ozon.py
+++ tests/test_ozon.py
@@ -1,4 +1,5 @@
-from App.lib import create_book
+from App.lib import create_book, add_book
+
 
 def test_create_book():
     books = {'title': 'Война и Мир', 'author': 'Лев Толстой', 'price': 500, 'tags': ['#война', '#любовь', '#толстой'], 'availability': True}
@@ -6,3 +7,10 @@ def test_create_book():
     result = create_book('Война и Мир', 'Лев Толстой', 500, ['#война', '#любовь', '#толстой'], True)
 
     assert books == result
+
+def test_add_book():
+    books = []
+    book = create_book('Война и Мир', 'Лев Толстой', 500, ['#война', '#любовь', '#толстой'], True)
+    add_book(books, book)
+    assert len(books) != 0
+    assert book in books
